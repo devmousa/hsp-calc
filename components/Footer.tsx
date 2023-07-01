0edc131522b9c47e7bd1f21c539fd782ecdbdd76
MasterMousa <mousshamed4@gmail.com>
2023-07-02 00:42:26 +0200

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="mt-8 mb-4 text-center md:text-2xl text-xl">
+    <footer className="mt-8 mb-4 p-2 text-center md:text-2xl text-xl">
       <p>تم تطوير هذا الموقع بواسطة المبرمج موسى أبوبكر</p>
       <Link className="text-blue-400 hover:text-blue-600" href="/about" >من أنا ؟</Link>
     </footer>
